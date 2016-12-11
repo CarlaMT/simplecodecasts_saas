0d3285a495378e467d92f246ef8dada073ed9d05
Carla Tyrell <catyrell@icloud.com>
2016-12-11 13:31:05 +0000

    Modified root in routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   devise_for :installs
   resources :contacts
   get '/about' => 'pages#about'
-  root 'pages#home'
+  root '/ '    => 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
